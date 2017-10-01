fde2dcdb4b31dd063643d0bb3c49cb7f14ad7848
Praseetha-KR <praseetha04@gmail.com>
2017-10-01 21:59:43 +0530

    Adds Developer, App & Screenshot models

--- appsearch/models.py
+++ appsearch/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
-# Create your models here.
+
+class Developer(models.Model):
+    name = models.CharField(max_length=30, null=True)
+    email = models.EmailField(null=True)
+
+
+class App(models.Model):
+    appid = models.CharField(max_length=30)
+    name = models.CharField(max_length=30)
+    desc = models.TextField(null=True)
+    icon = models.URLField()
+    rating = models.FloatField()
+    review_count = models.CharField(max_length=20)
+    current_version = models.CharField(max_length=20)
+    supported_os = models.CharField(max_length=20)
+    total_downloads = models.CharField(max_length=30)
+    published_date = models.DateField()
+    developer = models.ForeignKey(
+        'Developer', related_name='developer', null=True
+    )
+
+
+class Screenshot(models.Model):
+    app = models.ForeignKey('App', related_name='screenshots')
+    url = models.URLField()
