0a52cccb8471da57ff7835d0c05ac6212c6de197
Praseetha-KR <praseetha04@gmail.com>
2017-10-01 22:00:17 +0530

    Register appsearch models to admin

--- appsearch/admin.py
+++ appsearch/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import App, Developer, Screenshot
 
-# Register your models here.
+
+admin.site.register(App)
+admin.site.register(Developer)
+admin.site.register(Screenshot)
