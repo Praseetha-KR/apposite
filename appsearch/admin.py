3475b50667825fd00b0c11858adb01a1dc6adb49
Praseetha-KR <praseetha04@gmail.com>
2017-10-02 16:37:35 +0530

    Adds admin display fields

--- appsearch/admin.py
+++ appsearch/admin.py
@@ -2,6 +2,27 @@ from django.contrib import admin
 from .models import App, Developer, Screenshot
 
 
-admin.site.register(App)
-admin.site.register(Developer)
-admin.site.register(Screenshot)
+class AppAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'appid', 'tag_list', 'developer_email')
+
+    def tag_list(self, obj):
+        return ", ".join(o.slug for o in obj.tags.all())
+
+    def developer_email(self, obj):
+        return obj.developer.email
+
+
+class DeveloperAdmin(admin.ModelAdmin):
+    list_display = ('name', 'id', 'email')
+
+
+class ScreenshotAdmin(admin.ModelAdmin):
+    list_display = ('url', 'id', 'app_name')
+
+    def app_name(self, obj):
+        return obj.app.name
+
+
+admin.site.register(App, AppAdmin)
+admin.site.register(Developer, DeveloperAdmin)
+admin.site.register(Screenshot, ScreenshotAdmin)
